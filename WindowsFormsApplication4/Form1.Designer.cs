4006cfa19944fda3a63ade93b484a110c74d81a3
talshalti <talshalti1@gmail.com>
2013-07-06 18:25:27 +0300

    commit

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(572, 391);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(572, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(590, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 448);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(658, 51);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Page count = 0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(590, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(590, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(590, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 35);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(590, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(590, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 504);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Page count = 0";
+            label2.Text = "Email count = 0";
+            label3.Text = "In stack = 0";
+            label4.Text = "Found now = 0";
+            label5.Text = "Depth = 0";
+            linkLabel1.Text = "";
+        }
+
+        public ISet<Uri> GetNewLinks(string content, Uri sender)
+        {
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            ISet<Uri> newLinks = new HashSet<Uri>();
+            Uri s;
+            foreach (var match in regexLink.Matches(content))
+            {
+                s = new Uri(sender, match.ToString());
+                if (!newLinks.Contains(s))
+                    newLinks.Add(s);
+
+            }
+
+            return newLinks;
+        }
+        int pageCount = 0;
+        int emailCount = 0;
+        // 
+        public ISet<string> GetNewEmails(string content)
+        {
+            Regex regexLink = new Regex(@"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(co\.il|com)");
+
+            ISet<string> newLinks = new HashSet<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+
+            return newLinks;
+        }
+        Queue<MyUri> readQueue;
+        int maximumDepth = 10;
+        /// <summary>
+        /// Searches for emails from the hosts given in textBox1, and put all the emails in richTextBox1
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebRequest myWebRequest;
+            WebResponse myWebResponse;
+            Uri[] URLs = textBox1.Text.Split(',').Select(x => new Uri(x)).ToArray();
+            string Rstring;
+            MyUri URL;
+            HashSet<string> remembersEmails = new HashSet<string>();
+            foreach (Uri myURL in URLs)
+            {
+
+                HashSet<Uri> remembersUrls = new HashSet<Uri>();
+
+                readQueue = new Queue<MyUri>();
+                readQueue.Enqueue(new MyUri(myURL, 0));
+                while (readQueue.Count != 0 && emailCount < 1000)
+                {
+                    URL = readQueue.Dequeue();
+                    try
+                    {
+                        //if (URL.Uri.Host.IndexOf(".google.") != -1 && URL.Depth >= 3) continue;
+                        myWebRequest = WebRequest.Create(URL.Uri);
+                        myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
+
+                        Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet
+                        //and save it in the stream
+
+                        StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
+                        Rstring = sreader.ReadToEnd();//reads it to the end
+                        ISet<Uri> Links = GetNewLinks(Rstring, URL.Uri);//gets the links only
+                        ISet<string> Emails = GetNewEmails(Rstring);//gets the emails only
+                        foreach (string item in Emails)
+                        {
+                            if (remembersEmails.Add(item))
+                            {
+                                richTextBox1.Text += item + "\n";
+                                emailCount++;
+                            }
+                        }
+                        pageCount++;
+                        streamResponse.Close();
+                        sreader.Close();
+                        myWebResponse.Close();
+
+                        label1.Text = "Page count = " + pageCount;
+                        label2.Text = "Email count = " + emailCount;
+                        label3.Text = "In stack = " + readQueue.Count;
+                        label4.Text = "Found now = " + Emails.Count;
+                        label5.Text = "Depth = " + URL.Depth;
+                        linkLabel1.Text = URL.Uri.ToString();
+
+                        if (URL.Depth < maximumDepth)
+                        {
+                            foreach (Uri link in Links) //adds the current site links, if we haven't visited them yet, and if they have the same Host.
+                            {
+                                try
+                                {
+                                    if (link.Host == URL.Uri.Host && remembersUrls.Add(link))
+                                        readQueue.Enqueue(new MyUri(link, URL.Depth + 1));
+                                }
+                                catch (Exception e2)
+                                {
+                                    richTextBox2.Text += link + "\n";
+                                }
+                            }
+                        }
+                        Application.DoEvents(); // enables a clicking on the stop button.
+                    }
+                    catch (Exception e2)
+                    {
+                        richTextBox2.Text += e2.Message + "\n" + URL.Uri + " \n";
+                    }
+                }
+                richTextBox1.Text += "FINISHED";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readQueue.Clear();
+
+            label1.Text = "Page count = 0";
+            label2.Text = "Email count = 0";
+            label3.Text = "In stack = 0";
+            label4.Text = "Found now = 0";
+            linkLabel1.Text = "";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            maximumDepth = (int)numericUpDown1.Value;
+        }
+    }
+    public struct MyUri
+    {
+        public MyUri(Uri u, int d)
+        {
+            Uri = u;
+            Depth = d;
+        }
+
+        public Uri Uri;
+        public int Depth;
+    }
+}
